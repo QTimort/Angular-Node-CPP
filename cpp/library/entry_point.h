b13b8c820d7c19c0ada42c2224f6bc9a1931e8f4
Timothy Diguiet <timothy.diguiet@epitech.eu>
2021-03-18 08:23:39 +0100

    update cpp project to match with the native node module project sources

--- cpp/library/entry_point.h
+++ cpp/library/entry_point.h
@@ -2,11 +2,11 @@
 // Created by Timothy Diguiet on 18/03/2021.
 //
 
-#ifndef CPP_LIBRARY_H
-#define CPP_LIBRARY_H
+#ifndef CPP_ENTRY_POINT_H
+#define CPP_ENTRY_POINT_H
 
 #define EXPORT comment(linker, "/EXPORT:" __FUNCTION__ "=" __FUNCDNAME__)
 
 void EntryPoint(int argc, const char * argv);
 
-#endif //CPP_LIBRARY_H
+#endif //CPP_ENTRY_POINT_H
